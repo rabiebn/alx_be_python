6f58a449501be4658234cda42d3f411a6950729d
rabiebn <rabie.bnmr@gmail.com>
2025-07-09 20:20:10 +0100

    0: Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+# addition, subtraction, and multiplication
+
+number1 = 10
+number2 = 5
+
+print("Addition of ", number1, " and ", number2, " is ", number1 + number2)
+print("Subtraction of ", number1, " and ", number2, " is ", number1 - number2)
+print("Multiplication of ", number1, " and ", number2, " is ", number1 * number2)
