33f4536563660b73ec1e1520c4a3539782327807
rabiebn <rabie.bnmr@gmail.com>
2025-07-28 21:31:14 +0100

    0. Create a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+## Create a Simple Bank Account Class
+
+class BankAccount():
+    
+    def __init__(self, amount=0):
+        self.account_balance = amount
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            return False
+        self.account_balance -= amount
+        return True
+
+    def display_balance(self):
+        print(self.account_balance)
