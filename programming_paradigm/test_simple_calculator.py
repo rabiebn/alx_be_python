09d3013574b25d03ef4374573dececf5272cd3bd
rabiebn <rabie.bnmr@gmail.com>
2025-08-08 21:24:51 +0100

    2.2 changed methods name

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,17 +8,17 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(10, 2), 12)
         self.assertEqual(self.calc.add(22, 2), 24)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 2), 8)
 
-    def test_multiply(self):
+    def test_multiplying(self):
         self.assertEqual(self.calc.multiply(2, 5), 10)
         self.assertEqual(self.calc.multiply(2, -5), -10)
 
-    def test_divide(self):
+    def test_dividing(self):
         self.assertEqual(self.calc.divide(50, 5), 10)
         self.assertIsNone(self.calc.divide(1, 0))
