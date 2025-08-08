33b0181c9024ca2400b5499d57650a651ded3221
rabiebn <rabie.bnmr@gmail.com>
2025-08-08 22:29:32 +0100

    3. Implementing Basic OOP for a Library Management System

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,33 @@
+## 
+
+class Book():
+
+    _is_checked_out = False
+
+    def __init__(self, title, author):
+        self.author = author
+        self.title = title
+
+class Library():
+
+    _books = []
+
+    def add_book(self, newBook):
+        self._books.append(newBook)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if title == book.title and not book._is_checked_out:
+                book._is_checked_out = True
+                break
+
+    def return_book(self, title):
+        for book in self._books:
+            if title == book.title and book._is_checked_out:
+                book._is_checked_out = False
+                break
+
+    def list_available_books(self):
+        for book in self._books:
+            if not book._is_checked_out:
+                print(f"{book.title} by {book.author}")
